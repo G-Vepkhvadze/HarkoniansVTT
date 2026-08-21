4413e42210885b00eca67fd80b9209f9437ea442
G.Vepkhvadze <giorgi.vepkhvadze0@gmail.com>
2026-08-21 23:12:59 +0400

    Adjusting the pairing

---
+++ src/api/harkonians-gold.js
@@ -0,0 +1,64 @@
+import {
+    getGold,
+    syncGold
+} from "./harkonians-api.js";
+
+import {
+    getActorCredentials,
+    getWorldSecret,
+    isWorldLinked
+} from "../state.js";
+
+export async function fetchLinkedActorGold() {
+    if (!isWorldLinked()) {
+        throw new Error(
+            "Harkonians world is not linked."
+        );
+    }
+
+    const credentials =
+        getActorCredentials();
+
+    if (
+        !credentials.characterToken ||
+        !credentials.characterId ||
+        !credentials.foundryActorId
+    ) {
+        throw new Error(
+            "No Harkonians character is linked to this Foundry client."
+        );
+    }
+
+    return getGold(
+        getWorldSecret(),
+        credentials.foundryActorId
+    );
+}
+
+export async function synchronizeActorGold(
+    gold
+) {
+    if (!isWorldLinked()) {
+        throw new Error(
+            "Harkonians world is not linked."
+        );
+    }
+
+    const credentials =
+        getActorCredentials();
+
+    if (
+        !credentials.foundryActorId ||
+        !credentials.characterToken
+    ) {
+        throw new Error(
+            "No Harkonians character is linked."
+        );
+    }
+
+    return syncGold(
+        getWorldSecret(),
+        credentials.foundryActorId,
+        gold
+    );
+}
